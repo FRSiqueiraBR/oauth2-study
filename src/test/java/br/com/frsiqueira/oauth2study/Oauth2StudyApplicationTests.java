36bab92cc10b8f1c28429c25aaca39ab9de16d26
felipe.siqueira <felipe.siqueira@b2wdigital.com>
2020-03-06 15:29:46 -0300

    Initial commit

---
+++ src/test/java/br/com/frsiqueira/oauth2study/Oauth2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frsiqueira.oauth2study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
